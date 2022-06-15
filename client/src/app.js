d783c23f6f9d0f2d32013fdb7259b5a211730db8
vansia <dh608661@dal.ca>
2022-06-15 20:46:30 -0300

    Added cart feature

--- client/src/app.js
+++ client/src/app.js
@@ -4,7 +4,7 @@ import Header from "./components/header/header";
 
 import HomePage from "./pages/home-page";
 import ErrorPage from "./pages/error-page";
-
+import CartPage from "./pages/cart-page";
 import ProductPage from "./pages/product-details-page";
 import ProfilePage from "./pages/profile-page";
 
@@ -26,6 +26,9 @@ function App() {
         </Route>
         <Route exact path="/product/:id">
           <ProductPage />
+        </Route>
+        <Route exact path="/cart">
+          <CartPage />
         </Route>
         <Route component={ErrorPage} />
       </Switch>
