e164b51caa93e029bec57c5ea041d88d4d35d13a
subash <ablucky2717@gmail.com>
2022-06-15 20:21:47 -0300

    initial commit

--- config/db-connection.js
+++ config/db-connection.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "./.env" });
-const _DBUrl = process.env.DB;
+const _DBUrl =
+  process.env.DB ||
+  "mongodb+srv://subash:root@cluster0.cw4sfgd.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
   .connect(_DBUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }).then(() => console.log("Connection Successful")).catch((err) => console.log(`Error : ${err}`));
+  })
+  .then(() => console.log("Connection Successful"))
+  .catch((err) => console.log(`Error : ${err}`));
