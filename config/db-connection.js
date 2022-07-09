fc1af024887283115b6173fb7fdac4362e7e4685
Hemanth Nadipineni <54321hemanth@gmail.com>
2022-07-09 13:07:41 -0300

    comments

--- config/db-connection.js
+++ config/db-connection.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "./.env" });
-const _DBUrl = process.env.DB || "mongodb+srv://meghdootojha:meghdoot123@cluster0.rm5ca.mongodb.net/?retryWrites=true&w=majority";
+const _DBUrl = process.env.DB || "mongodb+srv://root:2580@cluster0.ygfbm.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
-  .connect(_DBUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connection Successful"))
-  .catch((err) => console.log(`Error : ${err}`));
+    .connect(_DBUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connection Successful"))
+    .catch((err) => console.log(`Error : ${err}`));
