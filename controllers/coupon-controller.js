b58b670ce258e402039c8aec405b3d148207a7ff
Hemanth Nadipineni <54321hemanth@gmail.com>
2022-07-14 01:54:19 -0300

    comments added

--- controllers/coupon-controller.js
+++ controllers/coupon-controller.js
@@ -1,5 +1,13 @@
+/**
+ * Author: Hemanth Nadipineni
+ * Banner ID: B00899473
+ * Model for Coupons
+ */
 const Coupon = require("../models/coupon-model");
 
+/**
+ * Method to list all the coupons
+ */
 const listCoupons = async (req, res) => {
   try {
     const coupons = await Coupon.find({});
@@ -9,56 +17,69 @@ const listCoupons = async (req, res) => {
   }
 };
 
+/**
+ * Method to add a coupon
+ */
 
 const addCoupon = async (req, res) => {
-    const couponData = {
-        couponCode:req.body.couponCode,
-        couponCondition:req.body.couponCondition,
+  const couponData = {
+    couponCode: req.body.couponCode,
+    couponCondition: req.body.couponCondition,
+    couponDiscount: req.body.couponDiscount,
+    maximumOff: req.body.maximumOff,
+  };
+  try {
+    const coupon = new Coupon(couponData);
+    await coupon.save();
+    res.json();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error });
+  }
+};
+
+/**
+ * Method to add a coupon by Id
+ */
+const updateCouponById = async (req, res) => {
+  try {
+    await Coupon.findByIdAndUpdate(
+      req.params.id,
+      {
+        couponCode: req.body.couponCode,
+        couponCondition: req.body.couponCondition,
         couponDiscount: req.body.couponDiscount,
-        maximumOff: req.body.maximumOff
-    };
-    try {
-      const coupon = new Coupon(couponData);
-      await coupon.save();
-      res.json();
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: error });
-    }
-  };
+        maximumOff: req.body.maximumOff,
+      },
+      { new: true }
+    );
+    res.json({
+      success: true,
+      message: "Coupon Updated",
+    });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
 
-  const updateCouponById = async (req, res) => {
-    try {
-      await Coupon.findByIdAndUpdate(req.params.id, {
-        couponCode:req.body.couponCode,
-        couponCondition:req.body.couponCondition,
-        couponDiscount: req.body.couponDiscount,
-        maximumOff: req.body.maximumOff
-    }, {new: true});
-      res.json({
-              success: true,
-              message: "Coupon Updated"
-      });
-    } catch (error) {
-      res.status(500).json({ error: error });
-    }
-  };
+/**
+ * Method to delete a coupon by Id
+ */
+const deleteCouponById = async (req, res) => {
+  try {
+    await Coupon.findByIdAndRemove(req.params.id);
+    res.json({
+      success: true,
+      message: "Coupon Deleted",
+    });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
 
-  const deleteCouponById = async (req, res) => {
-    try {
-      await Coupon.findByIdAndRemove(req.params.id);
-      res.json({
-              success: true,
-              message: "Coupon Deleted"
-      });
-    } catch (error) {
-      res.status(500).json({ error: error });
-    }
-  };
-
-  module.exports = {
-    listCoupons,
-    addCoupon,
-    updateCouponById,
-    deleteCouponById
-  };
+module.exports = {
+  listCoupons,
+  addCoupon,
+  updateCouponById,
+  deleteCouponById,
+};
